0cae8b087a8e4538e01efba30018b57038caf53c
kathrynzaro <zarokathryn@gmail.com>
2022-08-02 11:55:18 -0700

    tests 3 and 5 passing

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,6 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+
+select rental_date
+from customer inner join rental on customer.first_name = rental.rental_date
+where customer.first_name = 'Patricia';
--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,10 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+-- left join from film onto film_actor ??
+-- left join from film_actor onto actor ??
+
+SELECT title
+FROM film
+LEFT JOIN film_actor on film.film_id = film_actor.film_id
+LEFT JOIN actor on film_actor.actor_id = actor.actor_id
+WHERE actor.last_name = 'Davis';
