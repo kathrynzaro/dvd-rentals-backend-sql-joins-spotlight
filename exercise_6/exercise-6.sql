924515bb4ceea630a269351872dc61791f847bdb
kathrynzaro <zarokathryn@gmail.com>
2022-08-02 12:35:12 -0700

    ex 6

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,13 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+-- left join from film onto inventory ??
+-- left join from inventory onto rental ??
+-- left join from rental onto customer ??
+
+
+SELECT title
+FROM film
+LEFT JOIN inventory on film.film_id = inventory.film_id
+LEFT JOIN rental on inventory.inventory_id = rental.inventory_id
+LEFT JOIN customer on rental.customer_id = customer.customer_id
+WHERE customer.first_name = 'Roberta';
